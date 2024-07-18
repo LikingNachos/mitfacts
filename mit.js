6515cd8b2a12ebda70059179d33aab2e8fecf7d4
nilking23 <nlk@cowanandking.com>
2024-07-18 10:25:14 -0400

    Changes for Module 9

--- mit.js
+++ mit.js
@@ -1,7 +1,10 @@
 var mit = {
     city: 'Cambridge',
-    colors: ['pink', 'red'],
+    //colors: ['pink', 'red'],
+    colors: "Silver Gray",
     mascot: "robot",
-    founded: 1900,
-    motto: 'Carpe diem',
+    //founded: 1900,
+    founded: 1874,
+    //motto: 'Carpe diem',
+    motto: "Mens"
 };
